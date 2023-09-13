b6eb320fdc39b8e528da49559c74b2e5e1c1e245
sanstale6 <132974748+sanstale6@users.noreply.github.com>
2023-09-13 22:56:30 +0800

    更新 game.js

--- game.js
+++ game.js
@@ -11,35 +11,34 @@ document.addEventListener('DOMContentLoaded', function() {
   const gameMap = document.getElementById('game-map');
   const player = document.getElementById('player');
   const attackButton = document.getElementById('attack-button');
-  const moveButtons = document.getElementsByClassName('move-button');
 
-  // 玩家位置
+  // 玩家属性
+  let playerLevel = 1;
+  let playerHealth = 100;
+  let playerDamage = 10;
   let playerX = mapWidth / 2;
   let playerY = mapHeight / 2;
 
   // 敌人列表
   let enemies = [];
 
-  // 设置玩家位置
-  function setPlayerPosition(x, y) {
-    playerX = Math.max(0, Math.min(x, mapWidth - playerSize));
-    playerY = Math.max(0, Math.min(y, mapHeight - playerSize));
+  // 移动玩家
+  function movePlayer(x, y) {
+    playerX += x;
+    playerY += y;
     player.style.left = playerX + 'px';
     player.style.top = playerY + 'px';
 
     updateAttackableEnemies();
   }
 
-  // 移动玩家
-  function movePlayer(dx, dy) {
-    setPlayerPosition(playerX + dx, playerY + dy);
-  }
-
   // 更新可攻击的敌人
   function updateAttackableEnemies() {
+    const playerRect = player.getBoundingClientRect();
+
     for (const enemy of enemies) {
       const enemyRect = enemy.element.getBoundingClientRect();
-      const distance = getDistance(playerX, playerY, enemyRect.left, enemyRect.top);
+      const distance = getDistance(playerRect, enemyRect);
 
       if (distance <= attackRange) {
         enemy.element.classList.add('attackable-enemy');
@@ -49,9 +48,14 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   }
 
-  // 获取两个点之间的距离
-  function getDistance(x1, y1, x2, y2) {
-    return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+  // 获取两个矩形中心点之间的距离
+  function getDistance(rect1, rect2) {
+    const rect1X = rect1.left + rect1.width / 2;
+    const rect1Y = rect1.top + rect1.height / 2;
+    const rect2X = rect2.left + rect2.width / 2;
+    const rect2Y = rect2.top + rect2.height / 2;
+
+    return Math.sqrt(Math.pow(rect1X - rect2X, 2) + Math.pow(rect1Y - rect2Y, 2));
   }
 
   // 创建敌人
@@ -85,6 +89,11 @@ document.addEventListener('DOMContentLoaded', function() {
       enemy.y += dy;
       enemy.element.style.left = enemy.x + 'px';
       enemy.element.style.top = enemy.y + 'px';
+
+      // 敌人攻击玩家
+      if (Math.abs(playerX - enemy.x) < 25 && Math.abs(playerY - enemy.y) < 25) {
+        playerHealth -= Math.floor(Math.random() * 10) + 5;
+      }
     }
   }
 
@@ -96,9 +105,22 @@ document.addEventListener('DOMContentLoaded', function() {
     if (nearestEnemy) {
       const healthElement = nearestEnemy.getElementsByClassName('health')[0];
       let [currentHealth, maxHealth] = healthElement.innerText.split('/');
-      currentHealth = parseInt(currentHealth) - Math.floor(Math.random() * 20) - 10;
-      currentHealth = Math.max(currentHealth, 0);
+      const enemyHealth = parseInt(currentHealth) - Math.floor(Math.random() * 20) - 10;
+      currentHealth = Math.max(enemyHealth, 0);
       healthElement.innerText = currentHealth + '/' + maxHealth;
+      
+      if (enemyHealth <= 0) {
+        gameMap.removeChild(nearestEnemy);
+        enemies = enemies.filter(e => e.element !== nearestEnemy);
+        playerHealth += Math.floor(Math.random() * 20) + 10;
+
+        // 玩家升级
+        if (playerHealth >= 200) {
+          playerLevel++;
+          playerDamage += 5;
+          playerHealth = 100;
+        }
+      }
     }
   }
 
@@ -109,7 +131,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     for (const enemy of enemies) {
       const enemyRect = enemy.getBoundingClientRect();
-      const distance = getDistance(playerX, playerY, enemyRect.left, enemyRect.top);
+      const distance = getDistance(player.getBoundingClientRect(), enemyRect);
 
       if (distance < minDistance) {
         minDistance = distance;
@@ -120,34 +142,34 @@ document.addEventListener('DOMContentLoaded', function() {
     return nearestEnemy;
   }
 
-  // 移动按钮点击事件
-  for (const button of moveButtons) {
-    button.addEventListener('click', function() {
-      const direction = button.dataset.direction;
+  // 移动玩家事件监听
+  document.addEventListener('keydown', function(event) {
+    switch (event.keyCode) {
+      case 37: // Left arrow key
+        movePlayer(-5, 0);
+        break;
+      case 38: // Up arrow key
+        movePlayer(0, -5);
+        break;
+      case 39: // Right arrow key
+        movePlayer(5, 0);
+        break;
+      case 40: // Down arrow key
+        movePlayer(0, 5);
+        break;
+    }
+  });
 
-      switch (direction) {
-        case "up":
-          movePlayer(0, -5);
-          break;
-        case "down":
-          movePlayer(0, 5);
-          break;
-        case "left":
-          movePlayer(-5, 0);
-          break;
-        case "right":
-          movePlayer(5, 0);
-          break;
-      }
-    });
-  }
-
-  // 攻击按钮点击事件
+  // 攻击按钮事件监听
   attackButton.addEventListener('click', attackEnemy);
 
   // 游戏循环
   setInterval(function() {
     createEnemy();
     moveEnemies();
+
+    // 更新玩家信息
+    const playerInfo = "等级: " + playerLevel + " 血量: " + playerHealth + "/100 伤害: " + playerDamage;
+    document.title = playerInfo;
   }, 1000);
 });
