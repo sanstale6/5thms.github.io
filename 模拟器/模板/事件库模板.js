cd5f6e8ea4e32730568efe07dce55563a2d9ca1d
sanstale6 <132974748+sanstale6@users.noreply.github.com>
2023-09-14 20:53:08 +0800

    Add files via upload

---
+++ 模拟器/模板/事件库模板.js
@@ -0,0 +1,31 @@
+// 出生事件:当age等于0的时候,判断是否能出生,如果属性中心够就出生,不够就死亡.
+class 事件库模板{
+    constructor(){
+        this.执行列表 = []
+        this.name = '事件库目标'
+    }
+    运行列表(属性中心){
+        let pop = [];
+        this.执行列表.forEach(name=>{
+            let once = this[name](属性中心);
+            if(once) pop.push(name);
+        })
+        // 不要在遍历订阅的时候修改订阅列表,否则会导致遍历异常.
+        pop.forEach(name=>{
+            this.删除事件(name);
+        })
+    }
+    删除事件(name){
+        for(let i =0;i<this.执行列表.length;i++){
+            if(this.执行列表[i] == name){
+                this.执行列表.splice(i,1);
+            }
+        }
+    }
+    添加事件(name){
+        this.执行列表.push(name);
+    }
+    卸载自我(){
+        事件管理器.卸载事件库(this.name);
+    }
+}
